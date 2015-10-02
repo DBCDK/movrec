da9569314a58e39e1fe38c454147a7288bde8849
Jonas Peter Hyatt <jph@dbc.dk>
2015-10-02 21:21:07 +0200

    movieitemcontainer

---
+++ src/client/components/movieitemcontainer/MovieItemContainer.component.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import React, {PropTypes} from 'react';
+
+class MovieItemContainerComponent extends React.Component {
+
+  constructor(){
+    super();
+  }
+
+  render() {
+    return <div>movies go here!</div>;
+  }
+}
+
+MovieItemContainerComponent.displayName = 'MovieItemContainerComponent';
+MovieItemContainerComponent.propTypes = {
+  isAutoScrolling = PropTypes.bool
+};
+
+export default MovieItemContainerComponent;
