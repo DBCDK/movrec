abb35883990f2e0957e83dfb40ed2ecf99cd1018
Jonas Peter Hyatt <jph@dbc.dk>
2015-10-03 15:44:44 +0200

    animation

--- src/client/components/movieitemcontainer/VerticalMovieItemContainer.component.js
+++ src/client/components/movieitemcontainer/VerticalMovieItemContainer.component.js
@@ -19,20 +19,46 @@ class VerticalMovieItemContainerComponent extends MovieItemContainerComponent {
 
   leftHandler() {
     let like = this.props.addToLikes;
+    let moveAnimate = this.moveAnimate;
     return (elem, pid) => {
       let leftcol = document.getElementsByClassName('vertical-movie-item-container container-number-1')[0];
-      leftcol.insertBefore(elem, leftcol.firstChild);
+      // leftcol.insertBefore(elem, leftcol.firstChild);
+      moveAnimate(elem, leftcol);
       like(pid);
     }
   }
 
   rightHandler() {
     let dislike = this.props.addToDislikes;
+    let moveAnimate = this.moveAnimate;
     return (elem, pid) => {
       let rightcol = document.getElementsByClassName('vertical-movie-item-container container-number-3')[0];
-      rightcol.insertBefore(elem, rightcol.firstChild);
+      // rightcol.insertBefore(elem, rightcol.firstChild);
+      moveAnimate(elem, rightcol);
       dislike(pid);
     }
+  }
+
+  moveAnimate(element, newParent){
+    element = $(element);
+    newParent= $(newParent);
+
+    var oldOffset = element.offset();
+    element.appendTo(newParent);
+    var newOffset = element.offset();
+
+    var temp = element.clone().appendTo('body');
+    temp.css({
+      'position': 'absolute',
+      'left': oldOffset.left,
+      'top': oldOffset.top,
+      'z-index': 1000
+    });
+    element.hide();
+    temp.animate({'top': newOffset.top, 'left': newOffset.left}, 'fast', function(){
+      element.show();
+      temp.remove();
+    });
   }
 
   getMovieItems(movies) {
